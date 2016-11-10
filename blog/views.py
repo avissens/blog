427e287740b4434593ca73465e92f592b0ab6ba3
avissens <aviss@hotmail.co.uk>
2016-11-10 17:07:09 +0000

    Added: edit, delete

--- blog/views.py
+++ blog/views.py
@@ -25,11 +25,11 @@ def entries(page=1):
     entries = entries[start:end]
 
     return render_template("entries.html",
-        entries=entries,
-        has_next=has_next,
-        has_prev=has_prev,
-        page=page,
-        total_pages=total_pages
+        entries = entries,
+        has_next = has_next,
+        has_prev = has_prev,
+        page = page,
+        total_pages = total_pages
     )
     
 @app.route("/entry/add", methods=["GET"])
@@ -41,9 +41,39 @@ from flask import request, redirect, url_for
 @app.route("/entry/add", methods=["POST"])
 def add_entry_post():
     entry = Entry(
-        title=request.form["title"],
-        content=request.form["content"],
+        title = request.form["title"],
+        content = request.form["content"],
     )
     session.add(entry)
+    session.commit()
+    return redirect(url_for("entries"))
+    
+@app.route("/entry/<int:id>")
+def view_entry(id):
+    entry = session.query(Entry).get(id)
+    return render_template("entry.html", entry=entry)
+    
+@app.route("/entry/<int:id>/edit", methods=["GET"])
+def edit_entry_get(id):
+    entry = session.query(Entry).get(id)
+    return render_template("edit.html", entry=entry)
+    
+@app.route("/entry/<int:id>/edit", methods=["POST"])
+def edit_entry_post(id):
+    entry = session.query(Entry).get(id)
+    entry.title = request.form["title"]
+    entry.content = request.form["content"]
+    session.commit()
+    return redirect(url_for("entries"))
+
+@app.route("/entry/<int:id>/delete", methods=["GET"])
+def delete_entry(id):
+    entry = session.query(Entry).get(id)
+    return render_template("delete.html", entry=entry)
+    
+@app.route("/entry/<int:id>/delete", methods=["POST"])
+def delete_entry_post(id):
+    entry = session.query(Entry).get(id)
+    session.delete(entry)
     session.commit()
     return redirect(url_for("entries"))
