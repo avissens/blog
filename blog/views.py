2a175d46b1f1ae1b9a96c80abf2a8abc6b86ad23
avissens <aviss@hotmail.co.uk>
2016-11-11 09:42:05 +0000

    Login added

--- blog/views.py
+++ blog/views.py
@@ -32,17 +32,20 @@ def entries(page=1):
         total_pages = total_pages
     )
     
+from flask import request, redirect, url_for
+from flask.ext.login import login_required
+
 @app.route("/entry/add", methods=["GET"])
+@login_required
 def add_entry_get():
     return render_template("add_entry.html")
-    
-from flask import request, redirect, url_for
 
 @app.route("/entry/add", methods=["POST"])
+@login_required
 def add_entry_post():
     entry = Entry(
-        title = request.form["title"],
-        content = request.form["content"],
+        title=request.form["title"],
+        content=request.form["content"],
     )
     session.add(entry)
     session.commit()
@@ -77,3 +80,24 @@ def delete_entry_post(id):
     session.delete(entry)
     session.commit()
     return redirect(url_for("entries"))
+    
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+    
+from flask import flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
+from .database import User
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
